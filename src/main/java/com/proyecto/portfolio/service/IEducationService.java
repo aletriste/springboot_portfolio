957f192db15f8795f26902e6c65ac78fe0633c11
aletriste <atrsitezza@gmail.com>
2022-07-20 15:08:14 -0300

    proyecto completo de springboot

---
+++ src/main/java/com/proyecto/portfolio/service/IEducationService.java
@@ -0,0 +1,13 @@
+
+package com.proyecto.portfolio.service;
+
+import com.proyecto.portfolio.model.Education;
+import java.util.List;
+
+
+public interface IEducationService {
+    public List<Education> getEducation();
+    public void addEducation(Education education);
+    public void deleteEducation(Long id);
+    public void editEducation(Education education);
+}
